89ecfdeb9d8a62fc8597d845ffea605a41075839
IshaanShinde <ishaanshinde08@gmail.com>
2024-08-25 20:53:19 +0530

    hyprland not in login options

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,34 +1,35 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "ishaan";
-  home.homeDirectory = "/home/ishaan";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  home = {
+  username = "ishaan";
+  homeDirectory = "/home/ishaan";
+  stateVersion = "23.11";
+  };
+  
+  # enable unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
   home.packages = with pkgs; [
+  
+    # user applications
+    discord
+    brave
+    
+    
+    # utils
     vim
     zip
     unzip
     wget
+    vscode
     kitty
-    brave
     efibootmgr
+    
+    # for hyprland
+    xdg-desktop-portal-hyprland
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
   };
   
@@ -40,8 +41,11 @@
   programs.home-manager.enable = true;
 #--------------------------------------------------------------------------------------------------#
   imports = [
-    ./modules/git.nix
+    # hyprland
     ./modules/hyprland.nix
+    
+    # other
+    ./modules/git.nix
   ];
 #--------------------------------------------------------------------------------------------------#
 }
